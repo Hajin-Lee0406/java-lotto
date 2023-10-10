824bab7c60ae19a73d922d5a268024ea74e20855
Hajin-Lee0406 <glgkwls1@naver.com>
2023-10-10 16:18:14 +0900

    chore: mvc 패턴을 적용한 파일 생성

---
+++ src/main/java/lotto/lotto/controller/LottoController.java
@@ -0,0 +1,4 @@
+package lotto.lotto.controller;
+
+public class LottoController {
+}
---
+++ src/main/java/lotto/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.lotto.view;
+
+public class InputView {
+    private static final InputView instance = new InputView();
+
+    public static InputView getInstance() {
+        return instance;
+    }
+
+    private InputView() {
+    }
+
+}
