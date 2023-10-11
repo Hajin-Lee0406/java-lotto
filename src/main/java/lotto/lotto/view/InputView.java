c84c69a103178197d141c02bc961ea4dea35f2ad
Hajin-Lee0406 <glgkwls1@naver.com>
2023-10-11 16:57:34 +0900

    ref: view 함수 분리

--- src/main/java/lotto/lotto/view/InputView.java
+++ src/main/java/lotto/lotto/view/InputView.java
@@ -16,27 +16,35 @@ public class InputView {
     }
 
     public Integer getPurchaseAmount(){
+        System.out.println();
         System.out.println("구입금액을 입력해 주세요.");
+
         return Integer.parseInt(Console.readLine());
     }
 
     public List<Integer> getInputGoalNumber(){
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
 
-        List<Integer> inputList = new ArrayList<>();
-
-        String[] result = Console.readLine().split(",");
-
-        for (int i = 0; i < result.length; i++) {
-            inputList.add(Integer.parseInt(result[i]));
-        }
-
-        return inputList;
-
+        return stringToIntegerList(Console.readLine());
     }
 
     public Integer getInputBonusNumber(){
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
+
         return Integer.parseInt(Console.readLine());
+    }
+
+    private List<Integer> stringToIntegerList(String string){
+        List<Integer> inputList = new ArrayList<>();
+
+        String[] result = string.split(",");
+
+        for (String s : result) {
+            inputList.add(Integer.parseInt(s));
+        }
+
+        return inputList;
     }
 }
