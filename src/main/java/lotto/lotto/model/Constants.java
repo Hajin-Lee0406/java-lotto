374ece59a3a5014e8bdc11e1d21537362ff82042
Hajin-Lee0406 <glgkwls1@naver.com>
2023-10-11 16:49:52 +0900

    ref: 결과 출력 상수 처리

---
+++ src/main/java/lotto/lotto/model/Constants.java
@@ -0,0 +1,10 @@
+package lotto.lotto.model;
+
+public class Constants {
+    public static final String RESULT_GRADE_FIRST = "6개 일치 (2,000,000,000원) - ";
+    public static final String RESULT_GRADE_SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String RESULT_GRADE_THIRD = "5개 일치 (1,500,000원) - ";
+    public static final String RESULT_GRADE_FOURTH = "4개 일치 (50,000원) - ";
+    public static final String RESULT_GRADE_FIFTH = "3개 일치 (5,000원) - ";
+
+}
--- src/main/java/lotto/lotto/model/Result.java
+++ src/main/java/lotto/lotto/model/Result.java
@@ -4,16 +4,17 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static lotto.lotto.model.Constants.*;
+
 public enum Result {
-    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - "),
-    SECOND(5,30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD(5, 1500000, "5개 일치 (1,500,000원) - "),
-    FOURTH(4, 50000, "4개 일치 (50,000원) - "),
-    FIFTH(3, 5000, "3개 일치 (5,000원) - ");
+    FIRST(6, 2000000000, RESULT_GRADE_FIRST),
+    SECOND(5,30000000, RESULT_GRADE_SECOND),
+    THIRD(5, 1500000, RESULT_GRADE_THIRD),
+    FOURTH(4, 50000, RESULT_GRADE_FOURTH),
+    FIFTH(3, 5000, RESULT_GRADE_FIFTH);
 
     private final int hit;
     private final int account;
-
     private final String comment;
 
     Result(int hit, int account, String comment) {
